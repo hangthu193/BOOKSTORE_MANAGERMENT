7f00ffbdd301ace129f8f9b4cbddee1338bf32a2
phuonganh-27 <buipanh27@gmail.com>
2024-05-28 00:27:32 +0700

    update

--- app/src/main/java/com/example/book_store/Book.java
+++ app/src/main/java/com/example/book_store/Book.java
@@ -1,6 +1,8 @@
 package com.example.book_store;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ public class Book extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageButton btn_quaylai2;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_book);
@@ -19,6 +22,11 @@ public class Book extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn_quaylai2 = findViewById(R.id.btn_quaylai2);
+        btn_quaylai2.setOnClickListener(v -> {
+            Intent myIntent = new Intent(Book.this, MainActivity.class);
+            startActivity(myIntent);
         });
     }
 }
--- app/src/main/java/com/example/book_store/Bookdetail.java
+++ app/src/main/java/com/example/book_store/Bookdetail.java
@@ -1,8 +1,6 @@
 package com.example.book_store;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Bookdetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Button btn_forgotpass;
-        Button btn_sach;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bookdetail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btn_forgotpass = findViewById(R.id.btn_Forgotpass);
-        btn_forgotpass.setOnClickListener(v -> {
-            Intent myIntent = new Intent(MainActivity.this,Forgot_password.class);
-            startActivity(myIntent);
-
-        });
-        btn_sach = findViewById(R.id.btn_sach);
-        btn_sach.setOnClickListener(v -> {
-            Intent myIntent = new Intent(MainActivity.this, Book.class);
-            startActivity(myIntent);
-        });
     }
-
 }
