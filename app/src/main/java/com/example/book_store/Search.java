ce9875c0aadf2d6c3f3681336bf47e81696d3940
hangthu193 <hang193203@gmail.com>
2024-06-18 22:58:08 +0700

    add

--- app/src/main/java/com/example/book_store/Search.java
+++ app/src/main/java/com/example/book_store/Search.java
@@ -1,24 +1,31 @@
 package com.example.book_store;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Search extends AppCompatActivity {
+    CardView tacgiaCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        tacgiaCard = findViewById(R.id.tacgiaCard);
+        tacgiaCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Search.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
